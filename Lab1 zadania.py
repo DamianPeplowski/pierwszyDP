78e69b5f265d6c03b87882c366ee220e8e0b5bf6
Damian Pepłowski <93038194+DamianPeplowski@users.noreply.github.com>
2022-03-04 23:02:46 +0100

    Add files via upload

---
+++ Lab1 zadania.py
@@ -0,0 +1,75 @@
+from cmath import sin
+import math
+from re import S
+from tkinter import E
+
+#zad1
+a=5
+b=3
+print(a,b)
+
+imie='damian'
+nazwisko=' pepłowski'
+print(imie+nazwisko)
+
+lista1=[10,2,3424,33,2.3]
+lista2=["film","Diuna","Władca Pierścieni",]
+print(lista1)
+print(lista2)
+
+#zad 2
+c=8
+d=4
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(57//2)
+print(8 % 3)
+print(10 **4)
+print(pow(2,2))
+#zad3
+#zad4
+from math import e, fabs
+print(pow(e, 10))
+
+#nlog1 = math.sin.log(5+(sin **2)*8)**(1/6))
+#print(sin.log(5+(sin **2)*8)**(1/6)))
+
+print(math.fabs(3.55))
+print(math.fabs(4.80))
+
+#zad5
+Imie="DAMIAN"
+Nazwisko='PEPŁOWSKI'
+print(Imie.title())
+print(Nazwisko.title())
+
+cimie=imie.capitalize()
+cnazwisko=nazwisko.capitalize()
+
+print(cimie+cnazwisko)
+
+#zad6
+tekst="""Im covering my ears like a kid When your words mean nothing, 
+I go la la la Im turning up the volume when you speak Cause if my heart 
+cant stop it I find a way to block it, 
+I go La la, la la la la la na na na na na La la na na, la la la la la na na na na na"""
+
+print('Liczba pojawiających się la: ',tekst.count('la'))
+
+#zad7
+zmienna='Coś się kończy, coś się zaczyna'
+print(zmienna[1] + zmienna[30])
+#zad8
+print(zmienna.split(' '))
+#zad9
+s="345"
+print(s)
+f="0,23144524"
+
+
+
+
+
+ 
